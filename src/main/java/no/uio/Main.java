e5833288a3db47f71de9b1c470bf2b9215f2d7e7
marcooamato <amatomarco111@gmail.com>
2023-06-19 15:09:56 +0200

    Asset model loaded correctly

--- src/main/java/no/uio/Main.java
+++ src/main/java/no/uio/Main.java
@@ -1,7 +1,25 @@
 package no.uio;
 
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
+/**
+ * Model controller script.
+ * The main reads the asset model and waits for user input.
+ * The user input is then parsed and the corresponding action is executed.
+ * Possible actions are:
+ * - Add a new asset
+ * - Remove an existing asset
+ * - Update an existing asset
+ * - List all assets
+ * - List all assets of a specific type
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // read asset model into Jena model
+        Model model = ModelFactory.createDefaultModel();
+        model.read("asset_model.ttl");
+        // check if model is read correctly
+        model.write(System.out, "TURTLE");
     }
 }
